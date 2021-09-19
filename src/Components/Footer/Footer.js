19de3c3bdfa56b072adc2889c280c59fb5b1149a
anuragr07 <anuragr07@yahoo.com>
2021-09-18 18:00:03 -0700

    Footer design updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -9,38 +9,44 @@ export default function Footer(){
     return(
         <div className="my-footer">
         <Container >
-            <Row >
+            <Row xs={1} lg={4}>
                 <Col className="d-flex align-items-center justify-content-center">
                     <h3>
                         <Logo ></Logo>
                     </h3>
                 </Col>
                 <Col>
-                <h5>Menu</h5>
-                    <ul>
-                        <li><a href="index.html">Home</a></li>
-                        <li><a href="index.html">Web Designs</a></li>
-                        <li><a href="index.html">My work</a></li>
-                        <li><a href="index.html">My Resume</a></li>
-                        <li><a href="index.html">Contact</a></li>
-                    </ul>
+                    <div className='footer-links'>
+                    <h5>Menu</h5>
+                        <ul>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="index.html">Web Designs</a></li>
+                            <li><a href="index.html">My work</a></li>
+                            <li><a href="index.html">My Resume</a></li>
+                            <li><a href="index.html">Contact</a></li>
+                        </ul>
+                    </div>
                 </Col>
                 <Col>
-                <h5>My Work</h5>
-                    <ul>
-                        <li><a href="index.html">Website Designs</a></li>
-                        <li><a href="index.html">Ajax Forms</a></li>
-                        <li><a href="index.html">My Projects</a></li>
-                        <li><a href="index.html">Website Content</a></li>
-                    </ul>
+                    <div className='footer-links'>
+                    <h5>My Work</h5>
+                        <ul>
+                            <li><a href="index.html">Website Designs</a></li>
+                            <li><a href="index.html">Ajax Forms</a></li>
+                            <li><a href="index.html">My Projects</a></li>
+                            <li><a href="index.html">Website Content</a></li>
+                        </ul>
+                    </div>
                 </Col>
                 <Col>
-                <h5>Reach me at:</h5>
-                    <ul>
-                        <li><a href="index.html">Burnaby, BC V5A1C1</a></li>
-                        <li><a href="index.html">anuragr07@yahoo.com</a></li>
-                        <li><a href="index.html">+1 (778) 788-6322</a></li>
-                    </ul>
+                    <div className='footer-links'>
+                    <h5>Reach me at:</h5>
+                        <ul>
+                            <li><a href="index.html">Burnaby, BC V5A1C1</a></li>
+                            <li><a href="index.html">anuragr07@yahoo.com</a></li>
+                            <li><a href="index.html">+1 (778) 788-6322</a></li>
+                        </ul>
+                    </div>
                 </Col>
             </Row>
         </Container>
