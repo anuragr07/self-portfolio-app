49e443b7240dc54d4a69300a19e6f67dc526e9fd
anuragr07 <anuragr07@yahoo.com>
2022-02-09 23:09:23 -0800

    About section and heading style added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
-import "./style.scss";
+import "./Home.scss";
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import { Container } from "react-bootstrap";
 
 export default function Home(){
     return(
         <div>
+
+            {/* Banner starts here */}
             <Jumbotron fluid>
                 <div className="banner-img">
                     <div className="banner-text">
@@ -13,6 +16,20 @@ export default function Home(){
                     </div>
                 </div>
             </Jumbotron>
+            {/* Banner ends here */}
+
+            {/* About me starts here */}
+            <Container>
+                <div className="heading-1">
+                    <h1>About me</h1>
+                </div>
+                <div className="para-1">
+                    <p>2nd-year student in Post Baccalaureate Diploma in Computing and Information Systems acquiring knowledge in Modern web development frameworks and libraries including MERN stack, Spring Boot. Aiding users for the technical issues and developing quality solutions for the company while working in Information Systems team.</p>
+                </div>
+            </Container>
+            {/* About me ends here */}
+
+
         </div>
     )
 }
