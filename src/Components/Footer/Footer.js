7e6589b69852e7fa8554988c6d07bd1f2b2391a3
Anurag Rawal <anuragr07@yahoo.com>
2024-05-05 23:47:13 +0530

    made some design changes and added Copyright

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,9 @@ import './Footer.scss';
 
 // Fontawesome imports
 import Logo from '../Logo/Logo';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -18,6 +21,40 @@ export default function Footer() {
         { linkName: "Experience", linkVal: "experience" },
         { linkName: "Contact Me", linkVal: "contact-me" }
     ]
+
+    // Array for all links
+    const linkArray = [
+        {
+            linkName: 'Linkedin',
+            link: "https://www.linkedin.com/in/anuragr07/",
+            iconTag: <FontAwesomeIcon icon={faLinkedin} />
+        },
+        {
+            linkName: 'Github',
+            link: "https://github.com/anuragr07",
+            iconTag: <FontAwesomeIcon icon={faGithub} />
+        },
+        {
+            linkName: 'Instagram',
+            link:"https://www.instagram.com/anuragr07/",
+            iconTag: <FontAwesomeIcon icon={faInstagram} />
+        },
+        {
+            linkName: 'Email',
+            link: "mailto:anuragr07@yahoo.com",
+            iconTag: <FontAwesomeIcon icon={faEnvelope} />
+        },
+        {
+            linkName: 'Phone',
+            link: "tel:+919915428322",
+            iconTag: <FontAwesomeIcon icon={faPhoneAlt} />
+        },
+    ]
+
+    // generate links to use below
+    const links = linkArray.map((linkItem) => {
+        return <li><a href={linkItem.link}>{linkItem.iconTag}</a></li>
+    })
 
     // Function to get all the links 
     function getLinks() {
@@ -33,11 +70,11 @@ export default function Footer() {
             <Container>
                 <Row xs={1} lg={3}>
                     <Col className="d-flex align-items-center justify-content-center">
-                        <h3>
+                        <h1>
                             <Logo></Logo>
-                        </h3>
+                        </h1>
                     </Col>
-                    <Col>
+                    <Col className='d-flex justify-content-center'>
                         <div className='footer-links'>
                             <h5>Menu</h5>
                             <ul>
@@ -48,14 +85,19 @@ export default function Footer() {
                     <Col>
                         <div className='footer-links'>
                             <h5>Stay Connected</h5>
-                            <Form>
-                                <Form.Group className="mb-3 mt-4" controlId="exampleForm.ControlInput1">
-                                    <Row>
-                                        <Col xs={9}><Form.Control type="email" placeholder="name@example.com" className='footer-email'/></Col>
-                                        <Col xs={3}><Button variant="info" type="submit" className="submit-email">Submit</Button></Col>
-                                    </Row>
-                                </Form.Group>
-                            </Form>
+                            <Container>
+                                <Form>
+                                    <Form.Group className="mb-3 mt-4" controlId="exampleForm.ControlInput1">
+                                        <Row>
+                                            <Col xs={9}><Form.Control type="email" placeholder="name@example.com" className='footer-email'/></Col>
+                                            <Col xs={3}><Button variant="info" type="submit" className="submit-email">Submit</Button></Col>
+                                        </Row>
+                                    </Form.Group>
+                                </Form>
+                                <ul className='footer-links-social-media'>
+                                    {links}
+                                </ul>
+                            </Container>    
                         </div>
                     </Col>
                 </Row>
@@ -63,8 +105,8 @@ export default function Footer() {
             <Container >
                 <Row>
                     <Col>
-                        <div className='footer-bottom'>
-                            Anurag Rawal 2021
+                        <div className='footer-bottom d-flex align-items-center justify-content-center'>
+                            <p>Copyright &copy; Anurag Rawal 2024</p>
                         </div>
                     </Col>
                 </Row>
