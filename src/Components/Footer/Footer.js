ef63b8a32359b07baa4f5e31b912863b50c99c6c
Anurag Rawal <anuragr07@yahoo.com>
2024-05-07 20:22:03 +0530

    git added footer margin

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -66,7 +66,7 @@ export default function Footer() {
     }
 
     return (
-        <div className="my-footer">
+        <div className="my-footer mt-5">
             <Container>
                 <Row xs={1} lg={3}>
                     <Col className="d-flex align-items-center justify-content-center">
